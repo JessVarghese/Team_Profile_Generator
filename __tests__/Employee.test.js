fbc11e1342a4d4ff5aab90c219b4d8271fc06861
Jess Varghese <jessvarghese@gmail.com>
2021-12-03 10:54:21 -0500

    updated employee test to pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,12 @@
 const Employee = require("../lib/Employee");
-const employee = new Employee('Jane', '1', 'Jane@aol.com');
+const employee = new Employee('Jane', '1', 'Jane@aol.com', 'employee');
 
 
 test('creates a new employee object', () => {
     expect(employee.name).toBe('Jane');
     expect(employee.id).toBe('1');
     expect(employee.email).toBe('Jane@aol.com');
+    expect(employee.role).toBe('employee');
 });
 
 test('test getName() method', () => {
@@ -20,5 +21,10 @@ test('test getId() method', () => {
 
 test('test getEmail() method', () => {
     expect(employee.getEmail()).toBe('Jane@aol.com');
+
+});
+
+test('test getRole() method', () => {
+    expect(employee.getRole()).toBe('employee');
 
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,25 +1,26 @@
 const Engineer = require("../lib/Engineer");
-const engineer = new Engineer('bob', '2', 'bob@aol.com');
+
+const engineer = new Engineer('jane', '1', 'jane@aol.com', 'https://github.com/oh-my-fish');
 
 
 test('creates a new engineer object', () => {
-    expect(engineer.name).toBe('bob');
-    expect(engineer.id).toBe('2');
-    expect(engineer.email).toBe('bob@aol.com');
+    expect(engineer.name).toBe('jane');
+    expect(engineer.id).toBe('1');
+    expect(engineer.email).toBe('jane@aol.com');
 });
 
 test('test getName() method', () => {
-    expect(engineer.getName()).toBe('bob');
+    expect(engineer.getName()).toBe('jane');
 });
 
 
 test('test getId() method', () => {
-    expect(engineer.getId()).toBe('2');
+    expect(engineer.getId()).toBe('1');
 
 });
 
 test('test getEmail() method', () => {
-    expect(engineer.getEmail()).toBe('bob@aol.com');
+    expect(engineer.getEmail()).toBe('jane@aol.com');
 
 });
 
